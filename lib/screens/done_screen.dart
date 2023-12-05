c3c89d964c9a926c073eae9b61f7f0a48bea7d7d
InFAMO343USMind <46274707+InFAMO343USMind@users.noreply.github.com>
2023-12-05 10:19:25 -0600

    done screen done
    made done screen look like figma and modified the happy path test to go back to selection screen

--- lib/screens/done_screen.dart
+++ lib/screens/done_screen.dart
@@ -1,13 +1,51 @@
+//import 'dart:html';
 import 'package:flutter/material.dart';
+import 'package:homebrew_dripper/screens/recipe_selection_screen.dart';
 
 class DoneScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("enjoy your amazing handmade coffee", key: Key('done-screen-text')),
+          Expanded(
+            child: Align(
+              alignment: Alignment.center,
+              child: Text(
+                "enjoy your amazing handmade coffee",
+                textAlign: TextAlign.center,
+                key: Key('done-screen-text'),
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 18.0,
+                  color: Color(0xFF4C748B),
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            key: Key('done-button'),
+            onTap: () {
+              // Navigate to the selection page when the text is tapped.
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => RecipeSelectionScreen()),
+              );
+            },
+            child: Center(
+              child: Text(
+                'done',
+                style: TextStyle(
+                  fontFamily: 'Montserrat',
+                  fontSize: 18.0,
+                  color: Color(0xFF4C748B), // Change the text color to white
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
         ],
       ),
     );
