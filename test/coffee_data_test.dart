c26bee8e79ba3649e9e2b0c967b3c8131724dcb1
Royvencer <roy123villarreal@gmail.com>
2023-12-05 22:38:10 -0600

    added_tests_and_recipe

--- test/coffee_data_test.dart
+++ test/coffee_data_test.dart
@@ -15,4 +15,12 @@ void main() {
     expect(recipes[0].name, "Sweet Maria's");
     expect(recipes[0].steps, isA<List<RecipeStep>>());
   });
+  test('loadRecipes should give back the recipes in my app', () {
+    List<CoffeeRecipe> recipes = CoffeeData.loadRecipes();
+
+    // add whatever tests help give you confidence
+    expect(recipes[1], isA<CoffeeRecipe>());
+    expect(recipes[1].name, "PT's");
+    expect(recipes[1].steps, isA<List<RecipeStep>>());
+  });
 }
